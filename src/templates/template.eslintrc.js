469b3ed30644e2ad57cd4f7d02e919431b35c441
Erika Han <hanerika16@gmail.com>
2021-09-22 09:27:35 -0400

    some

---
+++ src/templates/template.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['avilatek-typescript'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-use-before-define': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+};
