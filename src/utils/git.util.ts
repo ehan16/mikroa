e7656908621f2a780f978c2336b3b5a12158a5ad
Erika Han <hanerika16@gmail.com>
2021-10-01 00:25:45 -0400

    :bug:

--- src/utils/git.util.ts
+++ src/utils/git.util.ts
@@ -1,37 +1,6 @@
-import inquirer from 'inquirer';
 import execa from 'execa';
 import { copy } from '../templates/default/default.template';
 import { showError, showSuccess } from './logger.util';
-
-export function askGithubCredentials() {
-  const questions = [];
-
-  questions.push({
-    type: 'input',
-    name: 'username',
-    message: 'Enter your Github username or e-mail address:',
-    validate(value: string) {
-      if (value.trim().length) {
-        return true;
-      }
-      return 'Please enter your username or e-mail address.';
-    },
-  });
-
-  questions.push({
-    type: 'password',
-    name: 'password',
-    message: 'Enter your password:',
-    validate(value: string) {
-      if (value.trim().length) {
-        return true;
-      }
-      return 'Please enter your password.';
-    },
-  });
-
-  return inquirer.prompt(questions);
-}
 
 export async function initGit(path: string) {
   const res = await execa('git', ['init'], {
