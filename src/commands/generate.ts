6321ea44e0657345546673fda1f392fe73413ad8
Erika Han <hanerika16@gmail.com>
2021-10-07 14:23:30 -0400

    try catch

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -26,48 +26,53 @@ export const handler = async (argv: Arguments<Options>) => {
 
   showTitle();
 
-  // 1. Check if microservice name was provided
-  if (microserviceName) {
-    if (
-      /[^A-Za-z0-9-]+/.test(String(microserviceName)) ||
-      /\s/.test(String(microserviceName))
-    ) {
-      showError('invalid name');
-      process.exit();
+  try {
+    // 1. Check if microservice name was provided
+    if (microserviceName) {
+      if (
+        /[^A-Za-z0-9-]+/.test(String(microserviceName)) ||
+        /\s/.test(String(microserviceName))
+      ) {
+        showError('invalid name');
+        process.exit();
+      }
+
+      const message = `${microservice || ''}`;
+      showGenerate(message);
+
+      // 1.1 In case the microservice name is passed, ask the user for language, orm, etc and then create the new microservice
+      const answers = await promptForOptions();
+      microservices.push({
+        name: microserviceName,
+        language: answers.language,
+        orm: answers.orm,
+        framework: answers.framework,
+      });
+    } else {
+      // 2. If not, then read root's config file with all the microservices config, push the object into the microservices array
+
+      // The JSON that looks like this
+      // {
+      //   'microservice-name': {
+      //     route: './microservice-name', ??????????
+      //     language: 'javascript',
+      //     orm: 'mongoose',
+      //     framework: 'express',
+      //   },
+      // };
+      const configFile = await readJson('config.json');
+      console.log('CONFIG FILE', configFile);
     }
 
-    const message = `${microservice || ''}`;
-    showGenerate(message);
+    // 3. Check in the cache which microservices haven't been created yet and filter them out
+    const cache = await readJson('cache.json');
 
-    // 1.1 In case the microservice name is passed, ask the user for language, orm, etc and then create the new microservice
-    const answers = await promptForOptions();
-    microservices.push({
-      name: microserviceName,
-      language: answers.language,
-      orm: answers.orm,
-      framework: answers.framework,
+    // 4. Create the ones that are not in the cache
+    microservices.forEach((_microservice) => {
+      console.log('Hey');
     });
-  } else {
-    // 2. If not, then read root's config file with all the microservices config, push the object into the microservices array
-
-    // The JSON that looks like this
-    // {
-    //   'microservice-name': {
-    //     route: './microservice-name', ??????????
-    //     language: 'javascript',
-    //     orm: 'mongoose',
-    //     framework: 'express',
-    //   },
-    // };
-    const configFile = await readJson('config.json');
-    console.log('CONFIG FILE', configFile);
+  } catch (e) {
+    showError('An error has ocurred while creating the microservice');
+    process.exit();
   }
-
-  // 3. Check in the cache which microservices haven't been created yet and filter them out
-  const cache = await readJson('cache.json');
-
-  // 4. Create the ones that are not in the cache
-  microservices.forEach((_microservice) => {
-    console.log('Hey');
-  });
 };
